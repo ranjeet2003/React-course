0f4261c9fd1d2efd156c6472d9f2cf65d9eccad6
ranjeet <1705213032@ietlucknow.ac.in>
2020-08-02 13:52:25 +0530

    representational and functional components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,13 +5,9 @@ import { Media } from 'reactstrap';
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          selectedDish: null
-        }
+        
     }
-    onDishSelect(dish){
-      this.setState({selectedDish:dish});
-    }
+    
     renderDish(dish){
       if(dish!=null){
         return(
@@ -19,7 +15,7 @@ class Menu extends Component {
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardBody>
               <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
+                <CardText>{dish.description}</CardText>
             </CardBody>
           </Card>
         );
@@ -34,12 +30,14 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={()=>this.onDishSelect(dish)}>
+                
+                <Card onClick={()=> this.props.onClick(dish.id)}>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                   </CardImgOverlay>
                 </Card>
+                
               </div>
             );
         });
@@ -48,9 +46,6 @@ class Menu extends Component {
           <div className="container">
             <div className="row">
                 {menu}
-            </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
