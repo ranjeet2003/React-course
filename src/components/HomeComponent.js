97ecd34c227b3ee7c5654b1d8ca8e56254efdcf8
ranjeet <1705213032@ietlucknow.ac.in>
2020-08-10 18:29:13 +0530

    first task of week 4 assignment

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,51 +7,45 @@ import {
     CardTitle,
     CardSubtitle
 } from 'reactstrap';
-import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
-import { FadeTransform } from 'react-animation-components';
+import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform} from 'react-animation-components';
 
 function RenderCard({item, isLoading, errMess}) {
 
-    if(isLoading) {
-        return(
-                <Loading />
+    if (isLoading) {
+        return (
+            <Loading/>);
+    } else if (errMess) {
+        return (
+            <h4>{errMess}</h4>
         );
-    }
-    else if (errMess) {
-        return(
-                <h4>{errMess}</h4>
-        );
-    }
-    else {
-return (
-    <FadeTransform in
-        transformProps={{
-        exitTransform: 'scale(0.5) translateY(-50%)'
-        }}>
-        <Card>
-            <CardImg src={
-                    baseUrl+item.image
+    } else {
+            return (
+            <FadeTransform in
+                transformProps={
+                    {exitTransform: 'scale(0.5) translateY(-50%)'}
+            }>
+            <Card>
+            <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{
+                        item.name
+                    }</CardTitle>
+                    {
+                    item.designation ? <CardSubtitle>{
+                        item.designation
+                    }</CardSubtitle> : null
                 }
-                alt={
-                    item.name
-                }/>
-            <CardBody>
-                <CardTitle>{
-                    item.name
-                }</CardTitle>
-                {
-                item.designation ? <CardSubtitle>{
-                    item.designation
-                }</CardSubtitle> : null
-            }
-                <CardText>{
-                    item.description
-                }</CardText>
-            </CardBody>
-        </Card>
-    </FadeTransform>);
-        }
+                    <CardText>{
+                        item.description
+                    }</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
+
+        );
+    }
 }
 
 function Home(props) {
@@ -59,19 +53,33 @@ function Home(props) {
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+                    <RenderCard item={
+                            props.dish
+                        }
+                        isLoading={
+                            props.dishesLoading
+                        }
+                        errMess={
+                            props.dishesErrMess
+                        }/>
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={
-                        props.promotion
-                    }
-                    isLoading={props.promosLoading} errMess={props.promosErrMess}
+                            props.promotion
+                        }
+                        isLoading={
+                            props.promoLoading
+                        }
+                        errMess={
+                            props.promoErrMess
+                        }/>
+                </div>
+                <div className="col-12 col-md m-1">
+                <RenderCard
+                        item={props.leader}
+                        isLoading={props.leaderLoading}
+                        errMess={props.leaderErrMess}
                     />
-                </div>
-                <div className="col-12 col-md m-1">
-                    <RenderCard item={
-                        props.leader
-                    }/>
                 </div>
             </div>
         </div>
