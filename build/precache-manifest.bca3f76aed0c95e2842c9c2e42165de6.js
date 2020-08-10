e28f05ae41efaf63c6dd8776992854e156034a34
Ranjeet Gautam <40926954+ranjeet2003@users.noreply.github.com>
2020-08-11 00:57:49 +0530

    Add files via upload

---
+++ build/precache-manifest.bca3f76aed0c95e2842c9c2e42165de6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9bddbcd9fc0ba060d601a14d8a577c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3d4ff96f73981f4b36",
+    "url": "/static/css/2.afaba3f7.chunk.css"
+  },
+  {
+    "revision": "70d72ed12a80e0e202eb",
+    "url": "/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "fa3d4ff96f73981f4b36",
+    "url": "/static/js/2.5ffa9fde.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.5ffa9fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d72ed12a80e0e202eb",
+    "url": "/static/js/main.2bf2467f.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
